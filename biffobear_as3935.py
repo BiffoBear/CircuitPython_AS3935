66f42a3c8c4890acf9ea8cf81c43e71c7183df14
BiffoBear <martin@martinstehens.net>
2021-02-25 17:23:42 +0700

    Implemented the as3935_spi function
    Completed the function that returns an instance of the AS3935 driver 
    with a SPI bus.

--- biffobear_as3935.py
+++ biffobear_as3935.py
@@ -79,8 +79,27 @@ _LIGHTNING_COUNT = (_0X01, _0X05, _0X09, _0X10)
 _FREQ_DIVISOR = (_0X10, _0X20, _0X40, _0X80)
 
 
-def as3935_spi(spi, cs, baudrate=None, *, interrupt_pin):
-    pass
+def as3935_spi(spi, cs, baudrate=1_000_000, *, interrupt_pin):
+    """Creates an instance of the Franklin AS3935 driver with a SPI connection.
+    
+    :param busio.SPI spi: The SPI bus connected to the chip.  Ensure SCK, MOSI, and MISO are
+        connected.
+    :param ~board.Pin cs: The pin connected to the chip's CS/chip select line.
+    :param ~board.Pin interrupt_pin: The pin connected to the chip's interrupt line. Note
+        that CircuitPython currently does not support interrupts, but the line is held high
+        for at least one second per event, so it may be polled. Some single board computers,
+        e.g. the Raspberry Pi, do support interrupts.
+    :param int baudrate: Defaults to 2,000,000 which is the maximum supported by the chip. If
+        another baudrate is selected, avoid +/- 500,000 as this will interfere with the chip's
+        antenna.
+
+    """
+    return AS3935(
+        bus=spi_dev.SPIDevice(
+            spi, digitalio.DigitalInOut(cs), baudrate=baudrate, polarity=1, phase=0
+        ),
+        interrupt_pin=interrupt_pin,
+    )
 
 
 def _reg_value_from_choices(value, choices):
--- tests/test_spi_and_i2c_methods.py
+++ tests/test_spi_and_i2c_methods.py
@@ -11,8 +11,54 @@ import pytest
 from CircuitPython_AS3935 import biffobear_as3935 as as3935
 
 
-def test_spi_setup_function_called_with_correct_args(mocker):
-    # Confirm that fucntion has correct calls
-    as3935.as3935_spi("spi", "cs", "baudrate", interrupt_pin="pin")
-    # Confirm that function can be called with a default baudrate
-    as3935.as3935_spi("spi", "cs", interrupt_pin="pin")
+@pytest.mark.parametrize(
+    "spi, cs_pin_in, cs_pin_out, baud, spibus, int_pin, as3935_inst",
+    [
+        (
+            "spi1",  # The SPI bus instance, arg for as3935_spi
+            "cs_pin_in1",  # The chip select pin (e.g. board.D5), arg for as3935_spi
+            "cs_pin_out1",  # The DigitalInOut object used in call to SPIDevice
+            "baud1",  # Baudrate optional arg for as3935_spi
+            "SPIbus1",  # SPIDevice used as arg for as3935_spi passed to AS3935
+            "int_pin1",  # Interrupt pin (e.g. board.D7), arg for
+            "as3935_inst1",  # Instance of AS3935 returned by as3935_spi
+        ),
+        (
+            "spi2",
+            "cs_pin_in3",
+            "cs_pin_out2",
+            "baud1",
+            "SPIbus2",
+            "int_pin2",
+            "as3935_inst2",
+        ),
+    ],
+)
+def test_as3935_instantiated_with_correct_args_from_as3935_spi(
+    mocker, spi, cs_pin_in, cs_pin_out, baud, spibus, int_pin, as3935_inst
+):
+    mock_cs_pin = mocker.Mock(name=cs_pin_in)
+    mock_digitalio = mocker.patch.object(
+        as3935.digitalio, "DigitalInOut", return_value=cs_pin_out
+    )
+    mock_spidevice = mocker.patch.object(
+        as3935.spi_dev, "SPIDevice", return_value=spibus
+    )
+    mock_as3935 = mocker.patch.object(as3935, "AS3935", return_value=as3935_inst)
+    as3935.as3935_spi(spi, mock_cs_pin, baud, interrupt_pin=int_pin)
+    # Check that cs pin converted to a DigitalInOut object
+    mock_digitalio.assert_called_once_with(mock_cs_pin)
+    # Confirm SPIDevice called with correct values
+    mock_spidevice.assert_called_once_with(
+        spi, cs_pin_out, baudrate=baud, polarity=1, phase=0
+    )
+    mocker.resetall()
+    result = as3935.as3935_spi(spi, mock_cs_pin, interrupt_pin=int_pin)
+    # Confirm that SPIDevice is called with a default baudreate
+    default_spi_baudrate = 1_000_000
+    mock_spidevice.assert_called_once_with(
+        spi, cs_pin_out, baudrate=default_spi_baudrate, polarity=1, phase=0
+    )
+    # Check that AS3935 instantiated with correct args
+    mock_as3935.assert_called_once_with(bus=spibus, interrupt_pin=int_pin)
+    assert result == as3935_inst
